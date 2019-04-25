e75ee18e8db103a9c3bf811221e4209ed0c4aa14
Yi ZHANG <z1joey@icloud.com>
2019-04-26 04:01:57 +0800

    A good example of singleton and GCD

--- PhotoManager.swift
+++ PhotoManager.swift
@@ -57,45 +57,141 @@ final class PhotoManager {
   private var unsafePhotos: [Photo] = []
   
   var photos: [Photo] {
-    
     var photosCopy: [Photo]!
-    
     concurrentPhotoQueue.sync {
         photosCopy = self.unsafePhotos
     }
-    
     return photosCopy
   }
   
   func addPhoto(_ photo: Photo) {
-    
     concurrentPhotoQueue.async(flags: .barrier) { [weak self] in
-        
+        // strongSelf
         guard let self = self else { return }
-        
         self.unsafePhotos.append(photo)
-        
         DispatchQueue.main.async { [weak self] in
             self?.postContentAddedNotification()
         }
     }
   }
   
+  // Better way to notify that all tasks are complete
   func downloadPhotos(withCompletion completion: BatchPhotoDownloadingCompletionClosure?) {
+
     var storedError: NSError?
-    for address in [PhotoURLString.overlyAttachedGirlfriend,
-                    PhotoURLString.successKid,
-                    PhotoURLString.lotsOfFaces] {
-                      let url = URL(string: address)
-                      let photo = DownloadPhoto(url: url!) { _, error in
-                        if error != nil {
-                          storedError = error
-                        }
-                      }
-                      PhotoManager.shared.addPhoto(photo)
+    let downloadGroup = DispatchGroup()
+    let addresses = [PhotoURLString.overlyAttachedGirlfriend,
+                     PhotoURLString.successKid,
+                     PhotoURLString.lotsOfFaces]
+    // use a queue with a .userInitiated quality of service for the concurrent calls.
+    let _ = DispatchQueue.global(qos: .userInitiated)
+    /*
+         Asynchronously execute different iterations
+         Only a good choice for concurrent queues that contain looping,
+         especially for keeping track of progress,
+         along with the appropriate stride length
+     */
+    DispatchQueue.concurrentPerform(iterations: addresses.count) { index in
+        let address = addresses[index]
+        let url = URL(string: address)
+        downloadGroup.enter()
+        let photo = DownloadPhoto(url: url!) { _, error in
+            if error != nil {
+                storedError = error
+            }
+            downloadGroup.leave()
+        }
+        PhotoManager.shared.addPhoto(photo)
     }
     
-    completion?(storedError)
+    // Serves as the asynchronous completion closure
+    downloadGroup.notify(queue: DispatchQueue.main) {
+        completion?(storedError)
+    }
+
+  }
+
+  // An example for canceling Dispatch Blocks
+    
+  func downloadPhotos1(withCompletion completion: BatchPhotoDownloadingCompletionClosure?) {
+    var storedError: NSError?
+    let downloadGroup = DispatchGroup()
+    var addresses = [PhotoURLString.overlyAttachedGirlfriend,
+                     PhotoURLString.successKid,
+                     PhotoURLString.lotsOfFaces]
+
+    addresses += addresses + addresses
+    
+    var blocks: [DispatchWorkItem] = []
+    
+    for index in 0..<addresses.count {
+        downloadGroup.enter()
+        
+        let block = DispatchWorkItem(flags: .inheritQoS) {
+            let address = addresses[index]
+            let url = URL(string: address)
+            let photo = DownloadPhoto(url: url!) { _, error in
+                if error != nil {
+                    storedError = error
+                }
+                downloadGroup.leave()
+            }
+            PhotoManager.shared.addPhoto(photo)
+        }
+        blocks.append(block)
+        
+        // executing on the main queue, but will execute later
+        DispatchQueue.main.async(execute: block)
+    }
+    
+    for block in blocks[3..<blocks.count] {
+        
+        let cancel = Bool.random()
+        if cancel {
+            // only cancel blocks still in a queue and have not executed
+            block.cancel()
+            downloadGroup.leave()
+        }
+    }
+    
+    downloadGroup.notify(queue: DispatchQueue.main) {
+        completion?(storedError)
+    }
+  }
+    
+  // A bad way to notify
+    
+  func downloadPhotos2(withCompletion completion: BatchPhotoDownloadingCompletionClosure?) {
+
+    DispatchQueue.global(qos: .userInitiated).async {
+        var storedError: NSError?
+
+        let downloadGroup = DispatchGroup()
+        for address in [PhotoURLString.overlyAttachedGirlfriend,
+                        PhotoURLString.successKid,
+                        PhotoURLString.lotsOfFaces] {
+                            let url = URL(string: address)
+
+                            // notify the group that a task has started
+                            downloadGroup.enter()
+                            let photo = DownloadPhoto(url: url!) { _, error in
+                                if error != nil {
+                                    storedError = error
+                                }
+                                // notify the group that this work is done
+                                downloadGroup.leave()
+                            }
+                            PhotoManager.shared.addPhoto(photo)
+        }
+
+        // block the current thread while waiting for tasks' completion
+        downloadGroup.wait()
+
+        // All image tasks have either completed or timed out
+        DispatchQueue.main.async {
+            completion?(storedError)
+        }
+    }
   }
   
   private func postContentAddedNotification() {
