0bbb7500f002f08619e437ec0ee8331bb9a68f29
Yi ZHANG <z1joey@icloud.com>
2019-06-19 23:48:01 +0800

    UIViewController Template

---
+++ ___FILEBASENAME___.swift
@@ -0,0 +1,23 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+    
+    // MARK:- IBOutlet properties
+    
+    // MARK:- IBOutlet actions
+    
+}
+
+
+// MARK:- Override methods
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
